202a25ec43998d34a51dc42c1779bc9d0f4e27c7
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-02-20 10:17:23 -0300

    Criação da Classe SubCategoria

---
+++ LojaVirtual/LojaVirtual/Models/SubCategoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class SubCategoria
+    {
+        public int SubCategoriaId { get; set; }
+
+
+        public string Nome { get; set; }
+
+
+        public string Descricao { get; set; }
+
+
+        public string Imagem { get; set; }
+
+
+        public string ImagemThumbnail { get; set; }
+
+
+        public bool Ativo { get; set; }
+
+
+        public bool Visivel { get; set; }
+
+        // relacão 1:N
+        public int CategoriaId { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
