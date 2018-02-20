051f34230d1552ac3939dc36f368d984a6653c2a
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-02-20 10:14:32 -0300

    Criaçao Classe Categoria

---
+++ LojaVirtual/LojaVirtual/Models/Categoria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+
+        public string Nome { get; set; }
+
+
+        public string Descricao { get; set; }
+
+
+        public string Imagem { get; set; }
+
+
+        public string ImagemThumbnail { get; set; }
+
+
+        public bool Ativo { get; set; }
+
+
+        public bool Visivel { get; set; }
+
+        // relacão 1:N
+        public int DepartamentoId { get; set; }
+
+        public Departamento Departamento { get; set; }
+    }
+}
